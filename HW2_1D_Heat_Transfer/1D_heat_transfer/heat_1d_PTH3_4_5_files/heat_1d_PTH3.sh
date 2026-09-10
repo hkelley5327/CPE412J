22772d9983c015d96e614356b49b6e711844d7f9
Hannah Kelley <100729525+hkelley5327@users.noreply.github.com>
2026-09-10 10:48:31 -0500

    Delete HW2_1D_Heat_Transfer directory

--- HW2_1D_Heat_Transfer/1D_heat_transfer/heat_1d_PTH3_4_5_files/heat_1d_PTH3.sh
+++
@@ -1,4 +0,0 @@
-./heat_1d_PTH3 12 20 1 > heat_1d_PTH3.txt
-./heat_1d_PTH3 12 20 2 >> heat_1d_PTH3.txt
-./heat_1d_PTH3 12 20 3 >> heat_1d_PTH3.txt
-./heat_1d_PTH3 12 20 4 >> heat_1d_PTH3.txt
--- HW2_1D_Heat_Transfer/1D_heat_transfer/heat_1d_serial_files/heat_1d_serial.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-./heat_1d_serial 12 20 > heat_1d_serial.txt
